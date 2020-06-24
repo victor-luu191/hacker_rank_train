b4df58fb0deaed8e18653eef309b2502be7b027f
Minh-Duc, LUU <victor.md.luu@gmail.com>
2020-06-23 21:02:42 -0700

    fix bugs & drop redundant methods
    + fixed finding fillable ranges
    + handling input correctly

--- medium/crossword.py
+++ medium/crossword.py
@@ -4,26 +4,7 @@ import os
 import re
 
 
-def find_start_non_plus(s):
-    for i in range(len(s)):
-        if s[i] != '+':
-            return i
-    return None
-
-
-def find_non_plus_ranges(s):
-    # return start and stop of the block of non-plus chars (i.e !='+')
-    # find index of the first non-plus letter
-    start = find_start_non_plus(s)
-    if start:
-        stop = len(s) - find_start_non_plus(s[::-1])
-        return start, stop
-    else:
-        return None, None
-
-
-# todo: use re.finditer() for this method
-def find_fillable_ranges(index, grid, axis=0):
+def find_fillable_ranges(axis, index, grid):
     # given position in a grid and a direction (axis),
     # return content and positions of fillable range
 
@@ -34,28 +15,25 @@ def find_fillable_ranges(index, grid, axis=0):
         cells = grid[index]
     else:
         print('\t finding fillable ranges in column', index)
-        cells = ''.join([grid[rr][index] for rr in range(len(grid))])
+        n_row = len(grid)
+        cells = ''.join([grid[rr][index] for rr in range(n_row)])
 
-    fillable_ranges = re.finditer("[-a-z]", cells)    # this one is still wrong
-    for m in fillable_ranges:
-        print(m.start(), '-', m.end(), m.group(0))
-
+    fillable_ranges = re.finditer("[^\+]+", ''.join(cells))
     return fillable_ranges
 
 
-# todo: modify this method to align with format of a range
-def fill_word_into_position(wrd, a_range, grid, axis=0):
+def fill_word_into_position(wrd, index, grid, a_range, axis=0):
     # fill given word into given pos in given grid
     # return grid filled with the word
 
-    index = a_range['index']
-    start, end = a_range.start() , a_range.end()
+    start, end = a_range.start(), a_range.end()
 
     filled_grid = grid.copy()
     if axis == 0:  # fill into row
-        filled_grid[index][start:end] = wrd
+        for cc in range(start, end):
+            filled_grid[index][cc] = wrd[cc - start]
     else:  # fill to column
-        for rr in a_range(start, end):
+        for rr in range(start, end):
             filled_grid[rr][index] = wrd[rr - start]
 
     return filled_grid
@@ -63,34 +41,45 @@ def fill_word_into_position(wrd, a_range, grid, axis=0):
 
 def is_match(wrd, cells):
     # len match
-    if len(cells) != len(words):
-        print('len of word and usable cells not match')
+    if len(cells) != len(wrd):
+        print('len of word and fillable range not match')
         return False
     # letter match: filled letters in the cells must match letters at the same positions in wrd
     for i in range(len(cells)):
         if cells[i] != '-':
             if cells[i] != wrd[i]:
-                print('letters at', i, 'positon of word and cells not match')
+                print('letters at', i, 'position of word and cells not match')
                 return False
     print('word and usable cells MATCH!')
     return True
 
 
-def try_filling(wrd, pos, grid, axis=0):  # axis 0 means row, 1 means col
+def make_crossword(grid):
+    # return crossword format from a 2d grid
+    row_strings = [''.join(rr) for rr in grid]
+    return '\n'.join(row_strings)
+
+
+def try_filling(wrd, index, grid, axis=0):  # axis 0 means row, 1 means col
     # a word can only be filled to a row/column (pos) if
     # i) len match: its length matches the no. of usable cells in pos
     # ii) letter match: its letters match with fixed letters in the row/column
 
-    fillable_ranges = find_fillable_ranges(pos, grid, axis)
-    for rr in fillable_ranges:
-        if is_match(wrd, rr):  # word and the range match, so can fit
-            filled_grid = fill_word_into_position(wrd, rr, grid, axis)
-            print(filled_grid)
+    fillable_ranges = find_fillable_ranges(axis, index, grid)
+    if not fillable_ranges:
+        pass
+
+    for a_range in fillable_ranges:
+        content = a_range.group(0)
+        # print('range to fit, start:', a_range.start(), 'end:', a_range.end(),
+        #       'content: ', content
+        #       )
+        if is_match(wrd, content):  # word and the (partial filled) content match, so can fit
+            filled_grid = fill_word_into_position(wrd, index, grid, a_range, axis)
+            print(make_crossword(filled_grid))
             return filled_grid
         else:
             return None
-
-    pass
 
 
 def crosswordPuzzle(grid, words):
@@ -112,9 +101,9 @@ def crosswordPuzzle(grid, words):
     # so try filling it into rows first, then try columns later
     # todo: (optimize) just jump to places where we can fit the  word
     w0 = words[0]
-
-    n_row, n_cols = len(grid), len(grid[0])
+    n_row, n_col = len(grid), len(grid[0])
     # try rows first
+    print('Try filling word ', w0, ' into rows:')
     for rr in range(n_row):
         grid_filled_with_the_word = try_filling(w0, rr, grid, axis=0)
         if grid_filled_with_the_word:  # successful fill
@@ -123,24 +112,29 @@ def crosswordPuzzle(grid, words):
                 return res
 
     # try filling into cols
-    for cc in range(n_cols):
+    print('Filling word ', w0, ' into rows is wrong. Now try filling it into columns:')
+    for cc in range(n_col):
         grid_filled_with_the_word = try_filling(w0, cc, grid, axis=1)
         if grid_filled_with_the_word:  # successful fill
             res = crosswordPuzzle(grid_filled_with_the_word, words[1:])
             if res:
                 return res
 
+    # if all fail
+    print('cannot fill word ', w0, ' back tracking')
+    pass
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    fptr = open("xword_output1.txt", 'w')
 
     crossword = []
 
     for _ in range(10):
-        crossword_item = input()
+        crossword_item = list(input())
         crossword.append(crossword_item)
 
-    words = input()
+    words = input().split(';')
 
     result = crosswordPuzzle(crossword, words)
 
