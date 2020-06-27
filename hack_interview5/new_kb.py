b6544db7fa4e07dec44bcc8fec6942f80960c21a
Minh-Duc, LUU <victor.md.luu@gmail.com>
2020-06-26 21:14:24 -0700

    Update new_kb.py

--- hack_interview5/new_kb.py
+++ hack_interview5/new_kb.py
@@ -10,6 +10,16 @@ import os
 # The function accepts STRING S as parameter.
 #
 
+def del_char_bf_cursor(cursor_pos, s):
+    if cursor_pos == 0:  # at start of string s
+        pass
+    else:  # delete prev char if have
+        if len(s) > 0:
+            prev_char = s[cursor_pos - 1]
+            s.remove(prev_char)
+        return s
+
+
 def receivedText(s):
     # s contains Latin letters, underscores, digits, and four special characters
     # the bugs with the kb will make the cursor jump around in the resulting string,
@@ -19,7 +29,6 @@ def receivedText(s):
     res = []
     cursor_pos = 0
     n_numlock_pressed = 0
-    is_numlock_on = True
     for i in range(len(s)):
         # handle special chars
         if s[i] == '*':
@@ -28,34 +37,21 @@ def receivedText(s):
             cursor_pos = len(res)
         if s[i] == '<':  # move to start of res string
             cursor_pos = 0
-        if s[i] == '#':
+        if s[i] == '#': # once numlock is pressed, it is stuck?
             n_numlock_pressed += 1
-            if n_numlock_pressed % 2 == 0:
-                is_numlock_on = True
-            else:
-                is_numlock_on = False
 
         if (s[i] not in special_chars) and not s[i].isdigit():  # insert normally
             res.insert(cursor_pos, s[i])
             cursor_pos += 1
-        # numlock funciton is reversed
-        if s[i].isdigit() and is_numlock_on:
+
+        if s[i].isdigit() and n_numlock_pressed == 0:
             res.insert(cursor_pos, s[i])
             cursor_pos += 1
-        if s[i].isdigit() and not is_numlock_on: # cannot use any number, so just skip it
+        if s[i].isdigit() and n_numlock_pressed > 0:  # cannot use any number, so just skip it
             pass
 
     return ''.join(res)
 
-
-def del_char_bf_cursor(cursor_pos, s):
-    if cursor_pos == 0:  # at start of string s
-        pass
-    else:  # delete prev char if have
-        if len(s) > 0:
-            prev_char = s[cursor_pos - 1]
-            s.remove(prev_char)
-        return s
 
 if __name__ == '__main__':
     # fptr = open(os.environ['OUTPUT_PATH'], 'w')
