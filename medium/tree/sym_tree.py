373fce8cdd2d9d0e9e406e20568f47e77bcfb953
Minh-Duc, LUU <victor.md.luu@gmail.com>
2020-08-11 15:48:10 -0700

    non-recursive solution, worked but slow in some cases

--- medium/tree/sym_tree.py
+++ medium/tree/sym_tree.py
@@ -5,57 +5,76 @@
 #     self.value = x
 #     self.left = None
 #     self.right = None
-def find_side(b, t):
-    # given a tree t and a boolean b, return the (values on) left-most (b=0) or right-most (b=1) side of t,
-    # starting from root
-    if b == 0:  # always follow left until terminal
-        # stop cond
-        if not t.left:  # t is leaf already
-            return [t]
-        left_most_side = [t] + find_side(b, t.left)
-        print('values on left most side:', get_values(left_most_side))
-        return left_most_side
-    else:
-        if not t.right:
-            return [t]
-        right_most_side = [t] + find_side(b, t.right)
-        print('values on right most side:', get_values(right_most_side))
-        return right_most_side
 
 
-def is_equal(side1, side2):
-    # check if resp values on two sides are equal
-    values1 = get_values(side1)
-    values2 = get_values(side2)
-    return values1 == values2
+def find_paths(t):
+    # given a binary tree, return all paths in t, each path represented as a binary str
+    # add root as start of all paths, then move either left or right, depend on which side exists,
+    # if left exists, find all left paths = [ '0' + p for p in paths in left sub tree]
+    # right paths = ['1' + p for p in paths in right sub tree]
+
+    # stop cond
+    if not t:
+        return []
+    if (not t.left) and (not t.right) :
+        return ['']
+
+    left_paths = ['0' + p for p in find_paths(t.left)]
+    right_paths = ['1' + p for p in find_paths(t.right)]
+    paths = left_paths + right_paths
+    return paths
 
 
-def get_values(side):
-    return [n.value for n in side]
+def make_mirror_path(p):
+    # given a path p represented as a binary str, return its mirror path by reversing its bits
+    mp = ''.join([str(1 - int(b)) for b in p])
+    return mp
+
+
+def get_values(p, t):
+    # return all values on path p of tree t
+    # add root value to the list, then follow the correct side specified by p[0] to the next node,
+    # repeat process
+    if not p:   # no more edge, means we are at leaf node already
+        if t:
+            return [t.value]
+        else:   # why there is this edge case?
+            return []
+
+    if p[0] == '0':
+        return [t.value] + get_values(p[1:], t.left)
+    if p[0] == '1':
+        return [t.value] + get_values(p[1:], t.right)
 
 
 def isTreeSymmetric(t):
     # symmetric around its center, i.e. each side mirrors the other.
 
-    # Recursive
-    # get the left-most side, by always go left until reach leaf node
-    # get the right-most side (sym side)
-    # If left-most == right-most:
-    # prune only leaf nodes from the tree, switch left --> right (things get ugly here)
-    # and repeat checking
-    # else: return false
+    # a side is a path from root to leaf, and each edge is either left (0) or right (1), so each side
+    # is a binary string.
+    # symmetric side of a given side s can be obtained by reverse each bit in s.
+    # For each side s on the left tree, check
+    # symm cond = its symmetric side exists in tree and contains same values at the nodes with s.
+    # i) if such condition is false, return false
+    # ii) else, move on to the next side
+    # If all edges satisfy the symmetric cond, return true
 
-    # stop cond
-    if not t:
-        return True
+    paths = find_paths(t)
+    print('paths in tree:', paths)
+    left_paths = [p for p in paths if p.startswith('0')]
+    right_paths = [p for p in paths if p.startswith('1')]
+    if not left_paths:
+        return not right_paths
 
-    left_most_side = find_side(0, t)
-    right_most_side = find_side(1, t)
-    if not is_equal(left_most_side, right_most_side) :
-        return False
-    # prune leaf nodes & continue checking
-    left_most_side[-1] = None
-    right_most_side[-1] = None
-    # print('tree after pruning leaf nodes:')
-    # print_tree(t)
-    return isTreeSymmetric(t)
+    for lp in left_paths:
+        rp = make_mirror_path(lp)
+        print('left path:', lp)
+        print('its mirror path:', rp)
+        left_values = get_values(lp, t)
+        right_values = get_values(rp, t)
+        print('left values:', left_values)
+        print('right values:', right_values)
+        sym_cond = (rp in paths) and (left_values == right_values)
+        if not sym_cond:
+            return False
+    return True
