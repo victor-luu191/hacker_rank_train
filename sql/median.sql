47626f682ef30bea265124c961b19eb9c03fde51
Minh-Duc, LUU <victor.md.luu@gmail.com>
2020-08-17 20:23:57 -0700

    try adding row number via loop-like, but not work

--- sql/median.sql
+++ sql/median.sql
@@ -1,7 +1,15 @@
-select @median_row:=floor(count(*)/2)
-from STATION;
+-- select @median_row:=floor(count(*)/2)
+-- from STATION;
+-- -- 
+-- select lat_n
+-- from STATION
+-- order by lat_n
+-- limit @median_row;
+
+set @median_row = (select floor(count(*)/2) from STATION);
 -- 
-select lat_n
+set @row = 0;
+select (@row := @row + 1) as row_number 
+    -- , lat_n
 from STATION
-order by lat_n
-limit @median_row;
+-- order by lat_n;
