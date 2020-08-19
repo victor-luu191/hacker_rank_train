5cfc6215b1635fa2314d450d6e30704458944a57
Minh-Duc, LUU <victor.md.luu@gmail.com>
2020-08-19 13:15:36 -0700

    speed up equal() method

--- medium/dynamic/equal_split.py
+++ medium/dynamic/equal_split.py
@@ -10,11 +10,16 @@ def find_uniq_values(arr):
             uniq_values.append(v)
     return uniq_values
 
+
 def equal(arr):
     # return min no. of ops needed to equalize all elements in arr
-    # in each operation, she can give x chocolates to some n-1 colleagues, where x in [1,2 or 5]
-    # so right bf the last op, n-1 colleagues must have the same chocolates,
-    # which equals to the chocolates of the remaining colleague minus x, so arr = [a-x, ..., a-x, a]
+    # Idea:
+    # Each time, must increase the smallest value min_v to reach some value, thus reducing at least
+    # 1 inequal.
+    # Consider two different values v and v+d, if we keep boosting them both, then the diff
+    # is always d (as each time we boost them with same value). And they keep being different.
+    # Thus, there must be some time that we start fixing the bigger one and boosting the smaller until
+    # it reach the larger. the number of ops needed will be min_ops(d)
 
     uniq_values = sorted(find_uniq_values(arr))
     min_v = uniq_values[0]
@@ -24,20 +29,30 @@ def equal(arr):
         return 0
 
     # else,
-    # must increase the smallest value min_v to reach some value,
-    #  thus reducing at least 1 inequal.
-    n_ops = []
-    for i in range(1, m):
-        value = uniq_values[i]
-        fix_idx = arr.index(value)
-        diff = value - min_v
-        new_arr = [arr[i] + diff for i in range(fix_idx)] + [arr[fix_idx]] + \
-                  [arr[i] + diff for i in range(fix_idx + 1, n)]
-        n_ops.append(cal_min_ops(diff) + equal(new_arr))
-    return min(n_ops)
+    next_value = uniq_values[1]
+    sort_arr = sorted(arr)
+    fix_idx = sort_arr.index(next_value)
+    diff = next_value - min_v
+    new_arr = (fix_idx + 1) * [sort_arr[fix_idx]] + \
+              [sort_arr[i] + diff for i in range(fix_idx + 1, n)]
+    return cal_min_ops(diff) + equal(new_arr)
+
+    # todo: speed up
+    # count = sort_arr.count(next_value)
+    # if m==2:
+    #     return count * cal_min_ops(diff)
+    # # there are at least 3 diff values: min_v, next_value and next_next_value, thus we first need to
+    # # convert min_v to next value
+    # # assume the next value occur k times, then we need to do k conversions
+    # k = count
+    # idx = k + fix_idx
+    # n_ops = k * cal_min_ops(diff)
+    # new_arr = idx * [next_value] + [sort_arr[j] + k * diff for j in range(idx, n)]
+    # return n_ops + equal(new_arr)
 
 
 def cal_min_ops(diff):
+    # in each operation, she can give x chocolates to some n-1 colleagues, where x in [1,2 or 5]
     i = diff // 5
     r = diff % 5
     j = r // 2
@@ -61,25 +76,3 @@ if __name__ == '__main__':
         fptr.write(str(result) + '\n')
 
     fptr.close()
-
-# # suppose a_i is the value for which a0 need least ops to reach , then
-# # we will make all colleagues with a0 bars become ai, thus reducing at least 1 inequal.
-# # Find such i
-# min_ops, best_i = -1, 0
-# for i in range(1, m):
-#     v = uniq_values[i]
-#     ops = cal_min_ops(v - min_v)
-#     if ops < min_ops:
-#         min_ops = ops
-#         best_i = i
-#
-# # we fix a single colleague with ai bars, and add to others ai - a0 bars,
-# # thus all colleagues with a0 bars become ai bars
-# sort_arr = sorted(arr)
-# value = uniq_values[best_i]
-# fix_idx = sort_arr.index(value)
-# diff = value - min_v
-# n = len(arr)
-# new_arr = [sort_arr[i] + diff for i in range(fix_idx)] + [sort_arr[fix_idx]] + \
-#           [sort_arr[i] + diff for i in range(fix_idx + 1, n)]
-# return min_ops + equal(new_arr)
